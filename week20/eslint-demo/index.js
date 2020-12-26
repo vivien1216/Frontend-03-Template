7f8ec6819e0abd46506bade985898f2e5d887d2d
vivien1216 <[Email@example.com](mailto:1870495688@qq.com)>
2020-12-26 15:54:08 +0800

    1

---
+++ week20/eslint-demo/index.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: vivien
+ * @Date: 2020-12-22 10:17:05
+ * @Last Modified by: vivien
+ * @LastEditTime: 2020-12-22 10:17:45
+ */
+let a = 'hello world';
+
+for(let i of [1,2,3]){
+    console.log(i);
+}
